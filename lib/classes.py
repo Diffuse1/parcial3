9259b49f860d35262ea02bf67de50b4b39093c08
annibal <annibalvillegas17@gmail.com>
2024-03-12 08:42:14 -0600

    12/03/2024

--- lib/classes.py
+++ lib/classes.py
@@ -3,4 +3,15 @@ class vertice():
         self.valor = valor
         pass
     pass
-        
+class arista(vertice):
+    def __init__(self, origen, destino,peso) -> None:
+        super().__init__(origen)
+        super().__init__(destino)
+        self.arista = { super().__init__(origen):{super().__init__(destino):peso}}
+    pass
+class grafo (arista):
+    def __init__(self, origen, destino, paso) -> None:
+        super().__init__(origen, destino, paso)
+    pass
+
+    
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from lib import *
 
 
-diccREL=()
+diccREL=(
+A={'B':5,'C':3},
+B={'A':5,'C':2,'D':4},
+C={'A':3,'B':2,'D':6,'D':7},
+D={'B':4,'C':6,'E':8},
+E={'C':7,'D':8}
+)
 diccREL['A']={'B':5,'C':3}
 diccREL['B']={'A':5,'C':2,'D':4}
 diccREL['C']={'A':3,'B':2,'D':6,'D':7}
 diccREL['D']={'B':4,'C':6,'E':8}
 diccREL['E']={'C':7,'D':8}
+printDicc(diccREL)
+verticeA = vertice('A')
+arista 
+
 
